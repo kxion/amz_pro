42826abc3a254c4f266d9bd67e1cdc5eaf3c8169
rhamill1 <rhamill11@gmail.com>
2016-11-21 11:31:01 -0800

    adds order count chart

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,6 +15,22 @@ class WelcomeController < ApplicationController
     clean_orders_hash = response.parse
     @clean_orders = clean_orders_hash["Orders"]["Order"]
 
+
+    date_count_hash = {}
+    @clean_orders.each { |order|
+      date_count_hash[order["PurchaseDate"]] = 1
+    }
+
+    @agr_date_count_hash = {}
+    date_count_hash.each { |order_date, date_count|
+      date_no_time = order_date[0..9]
+      if @agr_date_count_hash.key?(date_no_time) == false
+        @agr_date_count_hash[date_no_time] = 1
+      else
+        @agr_date_count_hash[date_no_time] += 1
+      end
+    }
+
   end
 
 end
