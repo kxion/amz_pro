e5266c87234912aee2bc8db2f5eeb0546d6d3173
rhamill1 <rhamill11@gmail.com>
2016-11-21 13:57:08 -0800

    adds total and formatting to summary figures

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'peddler'
 gem 'chartkick'
 gem 'groupdate'
+gem 'monetize'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,51 +1,76 @@
-require "peddler"
+require 'peddler'
 require 'json'
+require 'money'
 
 class WelcomeController < ApplicationController
 
   def index
-    client = MWS.orders(
-      primary_marketplace_id: "",
-      merchant_id: "",
-      aws_access_key_id: "",
-      aws_secret_access_key: "",
-    )
 
-    response = client.list_orders created_after: '2016-10-25'
-    clean_orders_hash = response.parse
-    @clean_orders = clean_orders_hash["Orders"]["Order"]
+    mws_call
 
+    format_order_count
 
-    date_count_hash = {}
-    @clean_orders.each { |order|
-      date_count_hash[order["PurchaseDate"]] = 1
-    }
+    format_order_sum
 
-    @agr_date_count_hash = {}
-    date_count_hash.each { |order_date, date_count|
-      date_no_time = order_date[0..9]
-      if @agr_date_count_hash.key?(date_no_time) == false
-        @agr_date_count_hash[date_no_time] = 1
-      else
-        @agr_date_count_hash[date_no_time] += 1
-      end
-    }
-
-    date_sum_hash = {}
-    @clean_orders.each { |order|
-      date_sum_hash[order["PurchaseDate"]] = order.dig("OrderTotal", "Amount").to_f
-    }
-
-    @agr_date_sum_hash = {}
-    date_sum_hash.each { |order_date, amount|
-      date_no_time = order_date[0..9]
-      if @agr_date_sum_hash.key?(date_no_time) == false && amount != nil
-        @agr_date_sum_hash[date_no_time] = amount
-      elsif amount != nil
-        @agr_date_sum_hash[date_no_time] += amount
-      end
-    }
+    sales_total
 
   end
+
+  private
+
+    def mws_call
+      client = MWS.orders(
+        primary_marketplace_id: "",
+        merchant_id: "",
+        aws_access_key_id: "",
+        aws_secret_access_key: "",
+      )
+
+      response = client.list_orders created_after: '2016-10-25'
+      clean_orders_hash = response.parse
+      @clean_orders = clean_orders_hash["Orders"]["Order"]
+    end
+
+    def format_order_count
+      date_count_hash = {}
+      @clean_orders.each { |order|
+        date_count_hash[order["PurchaseDate"]] = 1
+      }
+
+      @agr_date_count_hash = {}
+      date_count_hash.each { |order_date, date_count|
+        date_no_time = order_date[0..9]
+        if @agr_date_count_hash.key?(date_no_time) == false
+          @agr_date_count_hash[date_no_time] = 1
+        else
+          @agr_date_count_hash[date_no_time] += 1
+        end
+      }
+    end
+
+    def format_order_sum
+      date_sum_hash = {}
+      @clean_orders.each { |order|
+        date_sum_hash[order["PurchaseDate"]] = order.dig("OrderTotal", "Amount").to_f
+      }
+
+      @agr_date_sum_hash = {}
+      date_sum_hash.each { |order_date, amount|
+        date_no_time = order_date[0..9]
+        if @agr_date_sum_hash.key?(date_no_time) == false && amount != nil
+          @agr_date_sum_hash[date_no_time] = amount
+        elsif amount != nil
+          @agr_date_sum_hash[date_no_time] += amount
+        end
+      }
+    end
+
+    def sales_total
+      @total = 0
+      @agr_date_sum_hash.each { |order_date, amount|
+        @total += amount
+      }
+      @total = Money.us_dollar(@total * 100).format
+    end
 
 end
