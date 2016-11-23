3a7e6da877d8314a9d96a453449dd89683d91540
rhamill1 <rhamill11@gmail.com>
2016-11-22 20:26:58 -0800

    adds login verification methods to session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,11 +8,17 @@ module SessionsHelper
     session[:merchant_id]
   end
 
-  # def check_aws_login?
-  #   if session[:auth_login] == nil
-  #     false
-  #   else true
-  #   end
-  # end
+
+  def complete_login?
+    mws_login? == true && local_login? == true ? true : false
+  end
+
+    def mws_login?
+      session[:auth_hash] != nil ? true : false
+    end
+
+    def local_login?
+      session[:merchant_id] != nil ? true : false
+    end
 
 end
